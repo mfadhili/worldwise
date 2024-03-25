94604bca8528cc48f10b6b14e9f0eb4f973abe41
mfadhili <mfadhili@github.com>
2024-03-25 17:24:00 +0300

    Outlet

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from "./Sidebar.module.css"
 import Logo from "../logo/Logo.jsx";
 import AppNav from "../appNav/AppNav.jsx";
+import {Outlet} from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <Logo />
             <AppNav />
-            <p>List of cities</p>
+            <Outlet />
             <footer className={styles.footer}>
                 <p className={styles.copyright}>
                     &copy; Copyright {new Date().getFullYear()} by WorldWise INc.
