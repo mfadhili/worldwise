46bee1407aab0779ae4311644e8649991a004e83
mfadhili <mfadhili@github.com>
2024-04-01 02:32:14 +0300

    map over cities

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import styles from "./Map.module.css"
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {useCities} from "../../contexts/CitiesContext.jsx";
 
 function Map() {
 
@@ -10,6 +11,7 @@ function Map() {
     // LIKE A STATE HOOK, BUT GRABS PARAMS AND CAN UPDATE QUERY STRING
     const [searchParams,setSearchParams] = useSearchParams()
 
+    const {cities} = useCities();
     const [mapPosition, setMapPosition] = useState([40,0])
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
@@ -22,13 +24,15 @@ function Map() {
             <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map( city => (
+                    <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                        <Popup>
+                            <span>{city.emoji}</span> <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     );
