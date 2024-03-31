fcd24a1fa4ea4ba3cb484efeb48f69428adcd0a7
mfadhili <mfadhili@github.com>
2024-04-01 00:14:18 +0300

    current city highlighting working

--- src/components/city/cityItem.jsx
+++ src/components/city/cityItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./CityItem.module.css"
 import {Link} from "react-router-dom";
+import {useCities} from "../../contexts/CitiesContext.jsx";
 
 /*
 {
@@ -27,13 +28,14 @@ const formatDate = (date) =>
 function CityItem({city}) {
     // console.log(city)
     const {cityName, emoji,date, id, position} = city
+    const {currentCity} = useCities()
     // console.log(position)
 
 
     /* QUERY STRING CREATING URL THAT CAN HOLD "STATE"*/
     return (
         <li >
-            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
+            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active']: ''}`}>
                 <span className={styles.emoji}>{emoji}</span>
                 <h3 className={styles.name}>{cityName}</h3>
                 <time className={styles.date}>{formatDate(date)}</time>
