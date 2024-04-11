035414e22954f09a2b7bb181104f19ef9d59707c
mfadhili <mfadhili@github.com>
2024-04-11 19:15:39 +0300

    protected routes component

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import React, {useEffect} from 'react';
+import {useAuth} from "../contexts/AuthContext.jsx";
+import {useNavigate} from "react-router-dom";
+
+
+function ProtectedRoute({children}) {
+    const {isAuthenticated} = useAuth()
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/')
+        }
+    }, [isAuthenticated, navigate()]);
+
+    return children
+}
+
+export default ProtectedRoute;
